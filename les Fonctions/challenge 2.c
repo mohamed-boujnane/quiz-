bb8eefaaf0af8ebd50dced32f30ff29a83a41bbe
mohamed <mohamed.bouj.nane10@gmail.com>
2024-09-09 23:56:32 +0100

    im creat the dossie in every dossie i send challenge in inside the dossie

--- les Fonctions/challenge 2.c
+++ les Fonctions/challenge 2.c
@@ -1,20 +1,20 @@
 
 #include <stdio.h>
 
-int produit(int A , int B){
-    return A * B ;
+int produit(int A, int B)
+{
+    return A * B;
 }
-
 
 int main()
 {
-   int A, B;
-   printf("entre la premier entier :");
-   scanf("%d",&A);
-   printf("entre la 2 entier :");
-   scanf("%d",&B);
-   
-   printf("%d * %d = %d",A,B,produit(A,B));
-   
+    int A, B;
+    printf("entre la premier entier :");
+    scanf("%d", &A);
+    printf("entre la 2 entier :");
+    scanf("%d", &B);
+
+    printf("%d * %d = %d", A, B, produit(A, B));
+
     return 0;
 }
